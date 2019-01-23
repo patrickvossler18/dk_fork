743a41b27cc6038154f3b0d411223e65de9df97e
patrickvossler18 <patrick.vossler18@gmail.com>
2019-01-22 19:02:40 -0800

    add print statements to see if we are actually filtering out the data correctly

--- DeepKnockoffs/DeepKnockoffs/machine.py
+++ DeepKnockoffs/DeepKnockoffs/machine.py
@@ -387,18 +387,22 @@ class KnockoffMachine:
                 # Split the categorical and continuous variables
                 mXs_dis = mXs[:, self.cat_var_idx]
                 mXks_dis = mXks[:, self.cat_var_idx]
+                print(mXs_dis.shape)
 
                 mXs_cont = np.delete(mXs, self.cat_var_idx, 1)
                 mXks_cont = np.delete(mXks, self.cat_var_idx, 1)
+                print(type(mXs_cont))
+                print(mXs_cont.shape)
 
                 # Generate our weighting variable t
                 p_1 = len(self.cat_var_idx)
                 t_weight = np.max(1, self.kappa*((p - p_1)/p_1))
 
                 # Correlation between X and Xk
-                # If this doesn't work try just weighting the mean value?
                 corr_XXk_dis = (t_weight*mXs_dis*mXks_dis).mean(0)
+                print(corr_XXk_dis)
                 corr_XXk_cont = (mXs_cont*mXks_cont).mean(0)
+                print(corr_XXk_cont)
                 corr_XXk = corr_XXk_dis + corr_XXk_cont
             else:
                 corr_XXk = (mXs * mXks).mean(0)
