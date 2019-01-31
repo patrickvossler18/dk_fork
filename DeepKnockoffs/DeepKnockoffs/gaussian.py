00890af1302c84575c6e5edbcf069cf004093d53
patrickvossler18 <patrick.vossler18@gmail.com>
2019-01-31 11:41:38 -0800

    try regularizing sigma

--- DeepKnockoffs/DeepKnockoffs/gaussian.py
+++ DeepKnockoffs/DeepKnockoffs/gaussian.py
@@ -24,7 +24,7 @@ def solve_sdp(Sigma, tol=1e-3):
     # Convert the covariance matrix to a correlation matrix
     # Check whether Sigma is positive definite
     if(np.min(np.linalg.eigvals(Sigma))<0):
-        corrMatrix = cov2cor(Sigma + (1e-4)*np.eye(Sigma.shape[0]))
+        corrMatrix = cov2cor(Sigma + (1e-1)*np.eye(Sigma.shape[0]))
     else:
         corrMatrix = cov2cor(Sigma)
         
@@ -37,7 +37,7 @@ def solve_sdp(Sigma, tol=1e-3):
     
     #assert prob.status == cvx.OPTIMAL
     print(prob.status)
-    print(s)
+    print(s.value)
 
     s = np.clip(np.asarray(s.value).flatten(), 0, 1)
 	
